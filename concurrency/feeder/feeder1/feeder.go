0a2a6517bfed8d6594ba45fe7d6c403c5ef8a3fc
Dariusz Uchman <dariusz.uchman@snowsoftware.com>
2023-09-19 07:14:18 +0200

    feeder improved

--- concurrency/feeder/feeder1/feeder.go
+++ concurrency/feeder/feeder1/feeder.go
@@ -150,29 +150,65 @@ type merge struct {
 	errs    chan error
 }
 
-//func Merge(subs ...Subscription) Subscription {
-//
-//	for _, sub := range subs {
-//		go func(s Subscription) {
-//			for {
-//				var it Item
-//				select {
-//				case it = <-s.Updates():
-//				}
-//
-//				//select {
-//				//  case
-//				//}
-//			}
-//		}(sub)
-//	}
-//
-//	return nil
-//}
+func Merge(subs ...Subscription) Subscription {
+
+	m := &merge{
+		subs:    subs,
+		updates: make(chan Item),
+		quit:    make(chan struct{}),
+		errs:    make(chan error),
+	}
+
+	for _, sub := range subs {
+
+		go func(s Subscription) {
+
+			for {
+				var it Item
+
+				select {
+				case it = <-s.Updates():
+				case <-m.quit:
+					m.errs <- s.Close()
+					return
+				}
+
+				select {
+				case m.updates <- it:
+				case <-m.quit:
+					m.errs <- s.Close()
+					return
+				}
+
+			}
+
+		}(sub)
+	}
+
+	return m
+}
+
+func (m *merge) Updates() <-chan Item {
+	return m.updates
+}
+
+func (m *merge) Close() (err error) {
+	close(m.quit)
+	for _ = range m.subs {
+		if e := <-m.errs; e != nil { // HL
+			err = e
+		}
+	}
+	close(m.updates)
+	return
+}
 
 func main() {
 
-	subscription := Subscribe(Fetch("blog.golang.org"))
+	subscription := Merge(
+		Subscribe(Fetch("blog.golang.org")),
+		Subscribe(Fetch("googleblog.blogspot.com")),
+		Subscribe(Fetch("googledevelopers.blogspot.com")))
 
 	time.AfterFunc(3*time.Second, func() {
 		println("closed: ", subscription.Close())
